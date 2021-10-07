69732f504153450b4dc2127c3a95c33129276b9b
justinkroh22 <justinkroh22@gmail.com>
2021-10-07 13:50:51 -0400

    base structure

---
+++ src/main/java/com/example/fireadmin/FireadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.fireadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fireadmin/FireadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fireadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
